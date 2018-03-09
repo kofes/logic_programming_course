c710c654b898a265a3f761248fb687be8527d1b2
kofes <turnever@gmail.com>
2018-03-09 14:13:17 +1000

    rename 's' to 'simplifyOperation'

--- programs/diff.pl
+++ programs/diff.pl
@@ -49,130 +49,130 @@ simplify(X, X) :-
 simplify(X, R) :-
   X =..[OP, U],
   simplify(U, A),
-  s(OP, A, R).
+  simplifyOperation(OP, A, R).
 simplify(X, R) :-
   X =..[OP, U, V],
   simplify(U, A),
   simplify(V, B),
-  s(OP, A, B, R).
-% s helper
-s(-, X, R) :-
+  simplifyOperation(OP, A, B, R).
+% simplifyOperation helper
+simplifyOperation(-, X, R) :-
   number(X),
   R is -X.
-s(-, X, -X).
+simplifyOperation(-, X, -X).
 
-s(sin, 0, 0).
-s(sin, X, sin(X)).
-s(cos, 0, 1).
-s(cos, X, cos(X)).
-s(ln, 1, 0).
-s(ln, X, ln(X)).
+simplifyOperation(sin, 0, 0).
+simplifyOperation(sin, X, sin(X)).
+simplifyOperation(cos, 0, 1).
+simplifyOperation(cos, X, cos(X)).
+simplifyOperation(ln, 1, 0).
+simplifyOperation(ln, X, ln(X)).
 
-s(+, X, Y, R) :-
+simplifyOperation(+, X, Y, R) :-
   number(X),
   number(Y),
   R is X + Y.
-s(+, X, 0, X).
-s(+, 0, X, X).
-s(+, X, X, 2*X).
-s(+, X*Z, Y*Z, R*Z) :-
+simplifyOperation(+, X, 0, X).
+simplifyOperation(+, 0, X, X).
+simplifyOperation(+, X, X, 2*X).
+simplifyOperation(+, X*Z, Y*Z, R*Z) :-
   number(X),
   number(Y),
   R is X + Y.
-s(+, X*Y, Y, R*Y) :-
+simplifyOperation(+, X*Y, Y, R*Y) :-
   number(X),
   R is X + 1.
-s(+, Y, X*Y, R*Y) :-
+simplifyOperation(+, Y, X*Y, R*Y) :-
   number(X),
   R is X + 1.
-s(+, X^N, X^N, 2*X^N).
-s(+, -X^N, X^N, 0).
-s(+, X*Z^N, Y*Z^N, R*Z^N) :-
+simplifyOperation(+, X^N, X^N, 2*X^N).
+simplifyOperation(+, -X^N, X^N, 0).
+simplifyOperation(+, X*Z^N, Y*Z^N, R*Z^N) :-
   number(X),
   number(Y),
   R is X + Y.
-s(+, X*Y^N, Y^N, R*Y^N) :-
+simplifyOperation(+, X*Y^N, Y^N, R*Y^N) :-
   number(X),
   R is X + 1.
-s(+, Y^N, X*Y^N, R*Y^N) :-
+simplifyOperation(+, Y^N, X*Y^N, R*Y^N) :-
   number(X),
   R is X + 1.
 % TODO: add many conditions
-s(+, X, Y, X+Y).
+simplifyOperation(+, X, Y, X+Y).
 
-s(-, X, Y, R) :-
+simplifyOperation(-, X, Y, R) :-
   number(X),
   number(Y),
   R is X - Y.
-s(-, X, 0, X).
-s(-, 0, X, -X).
-s(-, X, X, 0).
-s(-, X*Z, Y*Z, R*Z) :-
+simplifyOperation(-, X, 0, X).
+simplifyOperation(-, 0, X, -X).
+simplifyOperation(-, X, X, 0).
+simplifyOperation(-, X*Z, Y*Z, R*Z) :-
   number(X),
   number(Y),
   R is X - Y.
-s(-, X*Y, Y, R*Y) :-
+simplifyOperation(-, X*Y, Y, R*Y) :-
   number(X),
   R is X - 1.
-s(-, Y, X*Y, R*Y) :-
+simplifyOperation(-, Y, X*Y, R*Y) :-
   number(X),
   R is 1 - X.
-s(-, X^N, X^N, 0).
-s(-, -X^N, X^N, -2*X^N).
-s(-, X*Z^N, Y*Z^N, R*Z^N) :-
+simplifyOperation(-, X^N, X^N, 0).
+simplifyOperation(-, -X^N, X^N, -2*X^N).
+simplifyOperation(-, X*Z^N, Y*Z^N, R*Z^N) :-
   number(X),
   number(Y),
   R is X - Y.
-s(-, X*Y^N, Y^N, R*Y^N) :-
+simplifyOperation(-, X*Y^N, Y^N, R*Y^N) :-
   number(X),
   R is X - 1.
-s(-, Y^N, X*Y^N, R*Y^N) :-
+simplifyOperation(-, Y^N, X*Y^N, R*Y^N) :-
   number(X),
   R is 1 - X.
 % TODO: add many conditions
-s(-, X, Y, X-Y).
+simplifyOperation(-, X, Y, X-Y).
 
-s(*, 0, _, 0).
-s(*, _, 0, 0).
-s(*, X, 1, X).
-s(*, 1, X, X).
-s(*, X, X, X^2).
-s(*, -X, X, -X^2).
-s(*, C*X, X, C*X^2) :-
+simplifyOperation(*, 0, _, 0).
+simplifyOperation(*, _, 0, 0).
+simplifyOperation(*, X, 1, X).
+simplifyOperation(*, 1, X, X).
+simplifyOperation(*, X, X, X^2).
+simplifyOperation(*, -X, X, -X^2).
+simplifyOperation(*, C*X, X, C*X^2) :-
   number(C).
-s(*, C, A/X, R/X) :-
+simplifyOperation(*, C, A/X, R/X) :-
   number(C),
   number(A),
   R is C * A.
-s(*, X^N, A/X, A*X^K) :-
+simplifyOperation(*, X^N, A/X, A*X^K) :-
   K is N-1,
   K =\= 1.
-s(*, X^N, A/X, A*X) :-
+simplifyOperation(*, X^N, A/X, A*X) :-
   K is N-1,
   K =:= 1.
-s(*, C*X, X^N, C*X^K) :-
+simplifyOperation(*, C*X, X^N, C*X^K) :-
   number(C),
   K is N+1.
-s(*, C*X^N, X, C*X^K) :-
+simplifyOperation(*, C*X^N, X, C*X^K) :-
   number(C),
   K is N+1.
-s(*, X, X^N, X^K) :-
+simplifyOperation(*, X, X^N, X^K) :-
   K is N+1.
-s(*, X^N, X, X^K) :-
+simplifyOperation(*, X^N, X, X^K) :-
   K is N+1.
 % TODO: add many conditions
-s(*, X, Y, X*Y).
+simplifyOperation(*, X, Y, X*Y).
 
-s(/, 0, _, 0).
-s(/, X, 1, X).
-s(/, X, X, 1).
-s(/, X*C, X, C).
-s(/, X, X*C, 1/C).
+simplifyOperation(/, 0, _, 0).
+simplifyOperation(/, X, 1, X).
+simplifyOperation(/, X, X, 1).
+simplifyOperation(/, X*C, X, C).
+simplifyOperation(/, X, X*C, 1/C).
 % TODO: add many conditions
-s(/, X, Y, X / Y).
+simplifyOperation(/, X, Y, X / Y).
 
-s(^, 0, _, 0).
-s(^, _, 0, 1).
-s(^, X, 1, X).
+simplifyOperation(^, 0, _, 0).
+simplifyOperation(^, _, 0, 1).
+simplifyOperation(^, X, 1, X).
 % TODO: add many conditions
-s(^, X, Y, X^Y).
+simplifyOperation(^, X, Y, X^Y).
