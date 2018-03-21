88004bb0713b9f47f00ed3eded902b4d8df303f3
kofes <turnever@gmail.com>
2018-03-21 15:58:00 +1000

    add writeln to queue simulation

--- programs/queue.pl
+++ programs/queue.pl
@@ -1,20 +1,31 @@
 #!/usr/bin/swipl
 
+% :- use_module(library(clpfd)).
+
 queue(S) :-
   queue(S, Q-Q).
 
-queue([enqueue(X)|Xs], Q) :-
-  enqueue(X, Q, Q1),
-  queue(Xs, Q1).
+queue([enqueue(X)|Xs], Qh-Qt) :-
+  enqueue(X, Qh-Qt, Q1h-Q1t),
+  determine(Q1h, L),
+  writeln([enqueue(X), acted |L]),
+  queue(Xs, Q1h-Q1t).
 
-queue([dequeue(X)|Xs], Q) :-
-  dequeue(X, Q, Q1),
-  queue(Xs, Q1).
+queue([dequeue(X)|Xs], Qh-Qt) :-
+  dequeue(X, Qh-Qt, Q1h-Q1t),
+  determine(Q1h, L),
+  writeln([dequeue(X), acted |L]),
+  queue(Xs, Q1h-Q1t).
 
 queue([], _).
 
 enqueue(X, Qh-[X|Qt], Qh-Qt).
 dequeue(X, [X|Qh]-Qt, Qh-Qt).
+
+determine([H|_], []) :- var(H), !.
+determine([], []).
+determine([H|T], [H|T1]) :-
+  determine(T, T1).
 
 test :-
   queue([enqueue(1), enqueue(2), enqueue(3), dequeue(X), dequeue(Y)]),
